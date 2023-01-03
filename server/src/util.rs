59cab6cc9ed6dfe8a53d0bd802fbedec521cfb20
Wilson Lin <code@wilsonl.in>
2023-01-03 15:16:00 +1100

    Fix repeated_copy algorithm

--- server/src/util.rs
+++ server/src/util.rs
@@ -45,12 +45,28 @@ pub async fn get_device_size(path: &Path) -> u64 {
 }
 
 pub fn repeated_copy<T: Copy>(dest: &mut [T], src: &[T]) {
+  assert_eq!(dest.len() % src.len(), 0);
+  assert!(dest.len() > src.len());
   dest[..src.len()].copy_from_slice(src);
 
-  let mut next = 0;
+  let mut next = src.len();
   while next < dest.len() {
     let end = min(next * 2, dest.len());
-    dest[next..end].copy_within(..end - next, next);
+    dest.copy_within(..end - next, next);
     next = end;
+  }
+}
+
+mod tests {
+  use super::repeated_copy;
+
+  #[test]
+  fn test_repeated_copy() {
+    let mut dest = vec![0u8; 25];
+    let src = [0, 1, 2, 3, 4];
+    repeated_copy(&mut dest, &src);
+    assert_eq!(dest, vec![
+      0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4,
+    ]);
   }
 }
