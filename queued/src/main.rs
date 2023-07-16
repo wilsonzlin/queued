a92d9598ef728f81dfb155fec92bb6a6fc6f32fd
Wilson Lin <code@wilsonl.in>
2023-07-16 15:42:24 +1000

    Allow listening on Unix socket

--- queued/src/main.rs
+++ queued/src/main.rs
@@ -5,7 +5,21 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 pub mod http;
 
 use crate::http::ctx::HttpCtx;
-use crate::http::start_http_server_loop;
+use crate::http::healthz::endpoint_healthz;
+use crate::http::metrics::endpoint_metrics;
+use crate::http::ops::endpoint_delete;
+use crate::http::ops::endpoint_poll;
+use crate::http::ops::endpoint_push;
+use crate::http::ops::endpoint_update;
+use crate::http::suspend::endpoint_get_suspend;
+use crate::http::suspend::endpoint_post_suspend;
+use crate::http::throttle::endpoint_get_throttle;
+use crate::http::throttle::endpoint_post_throttle;
+use axum::extract::DefaultBodyLimit;
+use axum::routing::get;
+use axum::routing::post;
+use axum::Router;
+use axum::Server;
 use cadence::Counted;
 use cadence::Gauged;
 use cadence::QueuingMetricSink;
@@ -22,8 +36,11 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::join;
+use tokio::net::UnixListener;
 use tokio::spawn;
 use tokio::time::sleep;
+use tokio_stream::wrappers::UnixListenerStream;
+use tracing::info;
 
 async fn sample_counter(
   statsd: &StatsdClient,
@@ -67,6 +84,9 @@ struct Cli {
   /// Port for server to listen on. Defaults to 3333.
   #[arg(long, default_value_t = 3333)]
   port: u16,
+
+  /// If provided, the server will create and listen on this Unix socket; `interface` and `port` will be ignored.
+  unix_socket: Option<PathBuf>,
 
   /// Optional StatsD server to send metrics to.
   #[arg(long)]
@@ -122,7 +142,52 @@ async fn main() {
         };
       }
     });
-  }
+  };
 
-  start_http_server_loop(cli.interface, cli.port, ctx).await;
+  let app = Router::new()
+    .route("/delete", post(endpoint_delete))
+    .route("/healthz", get(endpoint_healthz))
+    .route("/metrics", get(endpoint_metrics))
+    .route("/poll", post(endpoint_poll))
+    .route("/push", post(endpoint_push))
+    .route(
+      "/suspend",
+      get(endpoint_get_suspend).post(endpoint_post_suspend),
+    )
+    .route(
+      "/throttle",
+      get(endpoint_get_throttle).post(endpoint_post_throttle),
+    )
+    .route("/update", post(endpoint_update))
+    .layer(DefaultBodyLimit::max(1024 * 1024 * 128))
+    .with_state(ctx.clone());
+
+  match cli.unix_socket {
+    Some(socket_path) => {
+      let unix_listener =
+        UnixListener::bind(socket_path.clone()).expect("failed to bind UNIX socket");
+      let stream = UnixListenerStream::new(unix_listener);
+      let acceptor = hyper::server::accept::from_stream(stream);
+      info!(
+        unix_socket_path = socket_path.to_string_lossy().to_string(),
+        "server started"
+      );
+      Server::builder(acceptor)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    }
+    None => {
+      let addr = SocketAddr::from((cli.interface, cli.port));
+      info!(
+        interface = cli.interface.to_string(),
+        port = cli.port,
+        "server started"
+      );
+      Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    }
+  };
 }
